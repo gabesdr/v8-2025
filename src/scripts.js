8fd9070b046cafa9f0706bc60401a0e3a2cd585c
gabesdr <gabbidr00.03@gmail.com>
2025-10-22 17:31:04 +0000

    AHHHH JAVACSRIPTAAAHHHHH

--- src/scripts.js
+++ src/scripts.js
@@ -1,9 +1,39 @@
 /* TODO import á allt viðeigandi úr ./lib/todo.js */
+import { 
+  updateStats, 
+  createTodoItemElement, 
+  toggleTodoItemStatus, 
+  removeTodoItem, 
+  toggleFinished, 
+  clearList 
+} from "./lib/todo.js";
 
 /**
  * @param {HTMLElement} todolist
  */
 function initialize(todolist) {
+  const form = todolist.querySelector('.form')
+
+  if (!form) {
+    console.error('form fannst ekki, hætti')
+    return;
+  }
+
+  console.log(form)
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    if (!input) {
+      console.error('input fannst ekki');
+      return;
+    }
+
+    const value = input.value;
+
+    createTodoItem(todolist, value);
+    updateStats(todolist);
+  })
   /* TODO finna form */
   /* TODO setja submit event handler á form */
   /* TODO finna gildi textareits í formi innan event handlers og búa til todo item útfrá því */
